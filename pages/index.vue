1dbcd1fbe0fb91b1af95b6330ec4d2d508337c6c
Mikhail Delport <mikhail@delport.ca>
2018-09-27 09:16:01 -0400

    Initial commit of Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        pinboard-reader-pwa
+      </h1>
+      <h2 class="subtitle">
+        Pinboard Reader PWA
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
