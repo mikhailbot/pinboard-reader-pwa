40acc0b24a9c20bb70529b74f9a9d6876d9e9f73
Mikhail Delport <mikhail@delport.ca>
2018-11-17 12:17:56 -0500

    Fix iOS meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,8 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { mobileAppIOS: true },
-      { appleStatusBarStyle: 'black-translucent' }
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'appleStatusBarStyle', content: 'black-translucent' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
